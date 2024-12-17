e982096183a05080852ee30264917641ae3c6e26
Zahrotun <zahrotun0712@gmail.com>
2024-12-17 14:21:41 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,15 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_akademik";
+// Konfigurasi koneksi database
+$host = "localhost";      // Nama host (umumnya localhost)
+$username = "root";       // Username MySQL (umumnya root untuk local server)
+$password = "";           // Password MySQL (kosong untuk local server)
+$database = "db_perkantoran"; // Nama database yang baru dibuat
 
-// Buat koneksi
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Membuat koneksi
+$conn = new mysqli($host, $username, $password, $database);
 
-// Cek koneksi
+// Mengecek apakah koneksi berhasil
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
+?>
